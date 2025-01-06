09f858a48ac09f6a7ed87d65de5eadcbf9c0dd68
unknown <danilovieira37@icloud.com>
2025-01-06 14:32:21 -0300

    adicionando exercicio 7

--- exercicios.py
+++ exercicios.py
@@ -1,20 +1,29 @@
 # 6. Contagem de Palavras em Textos
 # Objetivo: Dado um texto, contar quantas vezes cada palavra única aparece nele.
 
-import re
+# import re
 
-texto = "a raposa marrom, salta sobre o cachorro preguiçoso. A raposa é muito ligeira."
+# texto = "a raposa marrom, salta sobre o cachorro preguiçoso. A raposa é muito ligeira."
 
-#excluir_virgula_e_ponto = texto.replace(",","")
-excluir_virgula_e_ponto = re.sub(r'[.,"\'-?:!;]', '', texto)
-palavras = excluir_virgula_e_ponto.split()
-contagem_palavras = {}
+# #excluir_virgula_e_ponto = texto.replace(",","")
+# excluir_virgula_e_ponto = re.sub(r'[.,"\'-?:!;]', '', texto)
+# palavras = excluir_virgula_e_ponto.split()
+# contagem_palavras = {}
 
-for palavra in palavras:
-    if palavra in contagem_palavras:
-        contagem_palavras[palavra] += 1
-    else:
-        contagem_palavras[palavra] = 1
+# for palavra in palavras:
+#     if palavra in contagem_palavras:
+#         contagem_palavras[palavra] += 1
+#     else:
+#         contagem_palavras[palavra] = 1
 
-print(contagem_palavras)
+# print(contagem_palavras)
 
+# 7. Normalização de Dados
+# Objetivo: Normalizar uma lista de números para que fiquem na escala de 0 a 1.
+
+numeros = [5, 20, 30, 45, 50]
+minimo = min(numeros)
+maximo = max(numeros)
+normalizados = [(n - minimo) / (maximo - minimo) for n in numeros]
+
+print(normalizados)
