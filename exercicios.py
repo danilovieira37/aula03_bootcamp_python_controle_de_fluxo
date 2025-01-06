10bd5783eaa150f6aed7759920a4a4fb5f2ba9dc
unknown <danilovieira37@icloud.com>
2025-01-06 14:58:40 -0300

    adicionando exercicio 8

--- exercicios.py
+++ exercicios.py
@@ -21,9 +21,22 @@
 # 7. Normalização de Dados
 # Objetivo: Normalizar uma lista de números para que fiquem na escala de 0 a 1.
 
-numeros = [5, 20, 30, 45, 50]
-minimo = min(numeros)
-maximo = max(numeros)
-normalizados = [(n - minimo) / (maximo - minimo) for n in numeros]
+# numeros = [5, 20, 30, 45, 50]
+# minimo = min(numeros)
+# maximo = max(numeros)
+# normalizados = [(n - minimo) / (maximo - minimo) for n in numeros]
 
-print(normalizados)
+# print(normalizados)
+
+# 8. Filtragem de Dados Faltantes
+# Objetivo: Dada uma lista de dicionários representando dados de usuários, filtrar aqueles que têm um campo específico faltando.
+
+usuarios = [
+    {"nome": "Alice", "email": "alice@example.com"},
+    {"nome": "Bob", "email": ""},
+    {"nome": "Carol", "email": "carol@example.com"}
+]
+
+usuarios_sem_email_valido = [usuario for usuario in usuarios if usuario["email"]=="" or usuario["nome"]==""]
+
+print(usuarios_sem_email_valido)
