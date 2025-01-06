5bbcff528e9dd3ebe1e46bdffbc27370644dbee6
unknown <danilovieira37@icloud.com>
2025-01-06 18:48:50 -0300

    adicionando exercicio 11

--- exercicios.py
+++ exercicios.py
@@ -49,19 +49,31 @@
 # 10. Agregação de Dados por Categoria
 # Objetivo: Dado um conjunto de registros de vendas, calcular o total de vendas por categoria.
 
-vendas = [
-    {"categoria": "eletrônicos", "valor": 1200},
-    {"categoria": "livros", "valor": 200},
-    {"categoria": "eletrônicos", "valor": 800}
-]
+# vendas = [
+#     {"categoria": "eletrônicos", "valor": 1200},
+#     {"categoria": "livros", "valor": 200},
+#     {"categoria": "eletrônicos", "valor": 800}
+# ]
 
-total_por_categoria = {}
-for venda in vendas:
-    categoria = venda["categoria"]
-    valor = venda["valor"]
-    if categoria in total_por_categoria:
-        total_por_categoria[categoria] += valor
-    else:
-        total_por_categoria[categoria] = valor
+# total_por_categoria = {}
+# for venda in vendas:
+#     categoria = venda["categoria"]
+#     valor = venda["valor"]
+#     if categoria in total_por_categoria:
+#         total_por_categoria[categoria] += valor
+#     else:
+#         total_por_categoria[categoria] = valor
 
-print(total_por_categoria)
+# print(total_por_categoria)
+
+# 11. Leitura de Dados até Flag
+# Objetivo: Ler dados de entrada até que uma palavra-chave específica ("sair") seja fornecida.
+
+dados = []
+entrada = ''
+while entrada.lower() != 'sair':
+    entrada = input('Digite algum dado (ou sair para fechar a aplicação): ')
+    if entrada.lower() != 'sair':
+        dados += entrada
+
+print(dados)
